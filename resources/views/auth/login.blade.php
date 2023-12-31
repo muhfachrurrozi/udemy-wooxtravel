f023d1eaad20523deb5a3469c7f46bb246fe6f83
muh.fachrurrozi <muh.fachrurrozi@gmail.com>
2023-12-31 15:46:08 +0700

    feat: login & register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -66,6 +66,53 @@
                         </div>
                     </form>
                 </div>
+            </div>
+        </div>
+    </div>
+</div> --}}
+
+<div class="reservation-form">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <form id="reservation-form"  method="POST" role="search" action="{{ Route('login') }}">
+                    @csrf
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <h4>Login</h4>
+                        </div>
+                        <div class="col-lg-12">
+                            <fieldset>
+                                <label for="Name" class="form-label">Your Email</label>
+                                <input id="email" placeholder="Email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </fieldset>
+                        </div>
+
+                        <div class="col-lg-12">
+                            <fieldset>
+                                <label for="Number" class="form-label">Your Password</label>
+                                <input id="password" placeholder="Password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </fieldset>
+                        </div>
+                        <div class="col-lg-12">
+                            <fieldset>
+                                <button type="submit" class="main-button">Login</button>
+                            </fieldset>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
